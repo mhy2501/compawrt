7402f530cc4a3294e790fb3e793b8563030703c3
mhy2501 <mylenehesguerra@gmail.com>
2023-02-08 14:06:09 +0800

    change button styling

--- frontend/src/pages/Ngos.jsx
+++ frontend/src/pages/Ngos.jsx
@@ -1,30 +1,28 @@
-
-import MyNavBar from '../components/MyNavBar'
-import { Organization } from '../components/Organization'
-import './Ngos.css'
+import MyNavBar from "../components/MyNavBar";
+import { Organization } from "../components/Organization";
+import "./Ngos.css";
 
 function Ngos() {
-    return (
-        <>
-         <MyNavBar />
-         <div className='org-cards'>
-         {Organization.map((org, index) => {
-            return (
-                <div className={org.cName} key={index}>
-                    <img className='org-img' src={org.img} alt="org logo" />
-                    <a href={org.volunteerUrl}>
-                        <button className='volunteer-btn'>Volunteer</button>
-                    </a>
-                    <a href={org.donateUrl}>
-                        <button className='donate-btn'>Donate</button>
-                    </a>
-                </div>
-            )
-
-         })}
-         </div>
-        </>
-    )
+  return (
+    <>
+      <MyNavBar />
+      <div className="org-cards">
+        {Organization.map((org, index) => {
+          return (
+            <div className={org.cName} key={index}>
+              <img className="org-img" src={org.img} alt="org logo" />
+              <a href={org.volunteerUrl}>
+                <button className="volunteer-btn">Volunteer</button>
+              </a>
+              <a href={org.donateUrl}>
+                <button className="donate-btn">Donate</button>
+              </a>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
-export default Ngos
+export default Ngos;
